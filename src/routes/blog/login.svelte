5987259f6c7338974c3e61cb4c3a841b39c402de
JReis23 <cjfreis23@gmail.com>
2022-04-08 21:01:47 +0200

    multiple changes in Update Posts

--- src/routes/blog/login.svelte
+++ src/routes/blog/login.svelte
@@ -13,20 +13,16 @@
 	const handleLogin = async () => {
 		try {
 			loading = true;
-			const {
-				user: userDetails,
-				session,
-				error
-			} = await supabase.auth.signIn({
+			const { user, session, error } = await supabase.auth.signIn({
 				email,
 				password
 			});
-			$user = userDetails;
+			// $user = userDetails;
 			if (error) throw error;
 			else {
 				goto('/blog');
 			}
-			return { userDetails, session, error };
+			return { user, session, error };
 		} catch (error) {
 			errLogin = error;
 			errLogin.message = `Adresse email ou mot de passe incorrect. Si tu es un nouveau utilisateur click sur "S'ENREGISTRER" pour te créer un compte.`;
